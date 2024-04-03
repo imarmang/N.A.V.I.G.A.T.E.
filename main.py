a5e0056941bc4fb126e8580398f6a69b83fb676d
Tornesal <brandon.decelle@gmail.com>
2024-04-03 15:38:20 -0400

    Implement user session management features and decorators
    The commit includes a custom decorator to check if a user is logged in or not, with redirection to the login page if necessary. Also, logout functionality has been added, which clears the session data for the user. Session checks have been added to various app routes, ensuring that the user is authenticated before accessing certain features or pages. Finally, a route has been added to retrieve courses from the database.

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template, redirect, url_for, request, session, j
 
 from flask_bcrypt import generate_password_hash, check_password_hash
 
+# This allows me to use my custom decorator with multiple functions and decorators
+from functools import wraps
+
 # Database access
 import pymongo
 
+
 app = Flask(__name__)
+
 
 # Database connection to fetch students database for student page
 client = pymongo.MongoClient("mongodb+srv://NAVIGATE:MQc0UmcHj4KZE3tP@navigate.l4xvkly.mongodb.net/")
@@ -13,9 +18,20 @@ database = client["NVGT"]
 students_collection = database["Students"]
 staff_collection = database["Staff"]
 
+
 # NOT USING THIS FOR SECURITY RIGHT NOW
 # Later, this will be a randomly assigned key. I have it static for testing purposes
 app.secret_key = 'testing'
+
+
+# Decorator to check if user is logged in. if not, they are redirected to log in page
+def check_logged_in(func):
+    @wraps(func)
+    def wrapper_func():
+        if 'n_number' not in session:
+            return redirect(url_for('student_login'))
+        return func()
+    return wrapper_func
 
 
 # Login page
@@ -46,11 +62,16 @@ def login_successful(username, password):
     # Checks if the username and password match the database
     if student["student_info"]["email"] == username and check_password_hash(student["student_info"]["password"], password):
         session['n_number'] = student['student_info']['nNumber']
-        # Print(session['n_number'])
         return True
     else:
         return False
 
+
+# Logout route
+@app.route('/logout')
+def logout():
+    session.pop('n_number', None)
+    return redirect(url_for('student_login'))
 
 # Flask Route to handle account creation
 @app.route('/create_account', methods = ['POST'])
@@ -101,11 +122,13 @@ def staff_login():
 
 # Home page WIP
 @app.route('/logged_in_home')
+@check_logged_in
 def logged_in_home():
     return render_template('logged_in_home.html')
 
 
 @app.route('/fetch_tutor_info')
+@check_logged_in
 def fetch_tutor_info():
     tutor_info = staff_collection.find_one({'student_info'})
 
@@ -113,6 +136,7 @@ def fetch_tutor_info():
 
 
 @app.route('/create_appointment', methods = ['GET', 'POST'])
+@check_logged_in
 def create_appointment():
 
     # Retrieving N# from session
@@ -124,7 +148,6 @@ def create_appointment():
         return redirect(url_for('student_login'))
 
     # Putting this in here for now
-    # TODO: Fix the name of the table
     appointment_collection = database["Appointments"]
 
     if request.method == 'POST':
@@ -144,17 +167,32 @@ def create_appointment():
 
 # To be used for creating appointments using the calendar.
 @app.route('/get_appointments', methods = ['GET'])
+@check_logged_in
 def calendar():
     appointment_collection = database["Appointments"]
     appointments = []
 
     # Only pull the columns I need from DB.
-    for appointment in appointment_collection.find({'nNumber': session['n_number']}, {'_id': 0, 'Appointment_date': 1, 'Course': 1, 'Subject': 1}):
+    for appointment in appointment_collection.find({'nNumber': session['n_number']},
+                                                   {'_id': 0, 'Appointment_date': 1,
+                                                    'Course': 1, 'Subject': 1}):
 
         appointments.append(appointment)
 
     # Returns the appointments list as JSON object
     return jsonify(appointments)
+
+
+@app.route('/courses', methods=['GET'])
+def get_courses():
+    courses_collection = database["Courses"]
+
+    # retrieves all courses
+    courses = courses_collection.find({}, {'_id': 0, 'Course_ID': 1, 'Subject': 1})
+    # creates list of courses formatted as dicts
+    courses_list = [course for course in courses]
+    print(courses_list)
+    return jsonify(courses_list)
 
 
 if __name__ == '__main__':
