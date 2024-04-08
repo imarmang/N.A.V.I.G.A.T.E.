bba8836071ab9463529866003d1e9df63eab4282
imarmang <agasparyan873@gmail.com>
2024-04-07 20:33:05 -0400

    Adding the course selector

--- static/script/home_page.js
+++ static/script/home_page.js
@@ -1,18 +1,22 @@
+// Helper function to toggle class 'active'
+function toggleClass(element, className) {
+    element.classList.toggle(className);
+}
+
 // Function used in main page to toggle the main menu
 const sideBar = document.querySelector('.side-bar');
 const toggleBtn = document.querySelector('.toggle-btn');
 
-
 toggleBtn.addEventListener('click', () => {
-    sideBar.classList.toggle('active');
+    toggleClass(sideBar, 'active');
 });
 
 function toggle(){
     var blur = document.getElementById('blur');
-    blur.classList.toggle('active');
-
     var popup = document.getElementById('popup');
-    popup.classList.toggle('active');
+
+    toggleClass(blur, 'active');
+    toggleClass(popup, 'active');
 }
 
 // Appear only if the Tutoring is chosen in the #popup
@@ -20,11 +24,7 @@ function showSecondDropdown(){
     var firstDropdown = document.getElementById("firstDropdown");
     var secondDropdown = document.getElementById("secondDropdown");
 
-    if (firstDropdown.value === "TTC") {
-        secondDropdown.style.display = "block";
-    } else {
-        secondDropdown.style.display = "none";
-    }
+    secondDropdown.style.display = firstDropdown.value === "TTC" ? "block" : "none";
 }
 
 // This method shows the appointment information
@@ -43,3 +43,33 @@ function showCourseInfo(courseInfo) {
         });
     });
 }
+
+$(document).ready(function() {
+    $('#firstDropdown').change(function() {
+        var serviceType = $(this).val();
+        if (serviceType === 'TTC') {
+            fetch('/get_student_courses')
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Failed to retrieve student courses data');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    var secondDropdown = $('#secondDropdown');
+                    secondDropdown.empty();
+                    secondDropdown.show();
+                    data.forEach(course => {
+                        secondDropdown.append('<option value="' + course.Course_ID + '">' + course.Subject + ' ' + course.Course_ID + '</option>');
+                    });
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    // Display an error message to the user
+                    alert('Failed to load student courses. Please try again later.');
+                });
+        } else {
+            $('#secondDropdown').hide();
+        }
+    });
+});
--- static/script/main.js
+++ static/script/main.js
@@ -1,26 +1,38 @@
-var a = document.getElementById("loginBtn");
-var b = document.getElementById("registerBtn");
-var x = document.getElementById("login");
-var y = document.getElementById("register");
+var inBtn = document.getElementById("signInBtn"); // a
+var upBtn = document.getElementById("signUpBtn");  // b
+var login = document.getElementById("login");  // x
+var register = document.getElementById("register");  // y
+var courses = document.getElementById("courses-container");
 
-// Sign In/Login function transition
-function login() {
-    x.style.left = "4px";
-    y.style.right = "-520px";
-    a.className += " white-btn";
-    b.className = "btn";
-    x.style.opacity = 1;
-    y.style.opacity = 0;
+// Helper function to handle transitions
+function handleTransition(element, position, opacity) {
+    element.style.right = position;
+    element.style.opacity = opacity;
 }
 
 // Sign up/Register function transition
-function register() {
-    x.style.left = "-510px";
-    y.style.right = "5px";
-    a.className = "btn";
-    b.className += " white-btn";
-    x.style.opacity = 0;
-    y.style.opacity = 1;
+function signUp() {
+    handleTransition(login, "-510px", "0");
+    handleTransition(register, "5px", "1");
+    handleTransition(courses, "-520px", "0");
+    inBtn.className = "btn";
+    upBtn.className += " white-btn";
+}
+
+// Continue to the choosing courses
+function continueBtn(){
+    handleTransition(login, "-520px", "0");
+    handleTransition(register, "-520px", "0");
+    handleTransition(courses, "5px", "1");
+}
+
+// Sign In/Login function transition
+function signIn() {
+    handleTransition(login, "4px", "1");
+    handleTransition(register, "-520px", "0");
+    handleTransition(courses, "-520px", "0");
+    inBtn.className += " white-btn";
+    upBtn.className = "btn";
 }
 
 // homeFunction transition
