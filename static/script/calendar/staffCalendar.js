9dfee55c16829c7334b7e0566187e5af1f3a3860
imarmang <agasparyan873@gmail.com>
2024-04-27 20:21:43 -0400

    Staff Calendar

--- static/script/calendar/staffCalendar.js
+++ static/script/calendar/staffCalendar.js
@@ -1,17 +1,41 @@
 $(document).ready(function() {
-    $('.calendar').fullCalendar({
-        header: {
-            left: 'month, agendaWeek, agendaDay',
-            center: 'title',
-            right: 'prev, today, next'
-        },
-        buttonText: {
-            today: 'Today',
-            month: 'Month',
-            week: 'Week',
-            day: 'Day',
-            prev: 'Prev',
-            next: 'Next'
-        },
+    fetch('/get_tutor_appointments',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+        .then(response => response.json())
+        .then(appointments => {
+            console.log(appointments);
+            $('.calendar').fullCalendar({
+                header: {
+                    left: 'month, agendaWeek, agendaDay',
+                    center: 'title',
+                    right: 'prev, today, next'
+                },
+                buttonText: {
+                    today: 'Today',
+                    month: 'Month',
+                    week: 'Week',
+                    day: 'Day',
+                    prev: 'Prev',
+                    next: 'Next'
+                },
+
+       // eventClick: function (event) {
+       //
+       //      // Show the details of the created event here, called from the calendar.js file
+       //      showAppInfo(event.title, event.start.format('YYYY-MM-DD'), event.start.format('HH:mm'), event.tutor);
+       //  },
+       events: appointments.map(appointment => ({
+            title: appointment.Subject + ' - ' + appointment.Course,
+            start: appointment.Appointment_date,
+            tutor: appointment.Tutor,
+            }))
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching appointments:', error);
     });
 });
